16395f46304c81a80aefc1169394e285f1a280e2
IrinaByck <ira002385@gmail.com>
2025-09-28 21:11:33 +0300

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "include/turkish.h"
+#include <string>
 
+int main()
+{
+    int number;
+    std::cout << "Введите число";
+    std::cin >> number;
+    std::cout << "result: " << convertNumberToTurkish(number) << std::endl;
+
+    return 0;
+}
