9fca07edd4acf453f1c52075da08897fccb20ff3
IrinaByck <ira002385@gmail.com>
2025-09-28 21:10:26 +0300

    Update turkish.cpp

--- lab1/src/turkish.cpp
+++ lab1/src/turkish.cpp
@@ -1,1 +1,16 @@
+#include "../include/turkish.h"
 
+std::string convertNumberToTurkish(int number)
+{
+    std::string num1[] = {"sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
+    std::string num2[] = {"sıfır", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
+    int number1 = number / 10;
+    int number2 = number % 10;
+    if (number2 == 0){
+        return num2[number1];
+    }
+    if (number1 ==0 ){
+        return num1[number2];
+    }
+    return num2[number1] + " " + num1[number2];
+}
